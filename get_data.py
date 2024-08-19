e1bf04dcee2f8c34755f214522697342f0b8d65b
corazon71 <gurunaths6102003@gmail.com>
2024-08-19 13:36:48 +0530

    get_data.py configured

---
+++ get_data.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import json
+
+from dotenv import load_dotenv
+
+import certifi
+import pandas as pd
+import numpy as np
+import pymongo
+
+from Threat_Lens.Exception.Exception import TLException
+from Threat_Lens.Logger.Logger import logging
+
+
+class DataExtract():
+  def __init__(self):
+    try:
+      pass
+    except Exception as e:
+      raise TLException(e, sys)
+    
+  def csv2json_converter(self):
+    try:
+      pass
+    except Exception as e:
+      raise TLException(e, sys)
+    
+  def pushing_Data2MongoDB():
+    try:
+      pass
+    except Exception as e:
+      raise TLException(e, sys)
+    
+  if __name__ == "__main__":
+    pass
+    
