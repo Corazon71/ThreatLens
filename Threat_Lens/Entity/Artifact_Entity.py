152ead0ee1c3b2e82573b3bf205db9338be7952b
corazon71 <gurunaths6102003@gmail.com>
2024-08-19 19:08:35 +0530

    Entity Files configured

---
+++ Threat_Lens/Entity/Artifact_Entity.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+@dataclass
+class TLDataIngestArtifact:
+  pass
+
+class TLDataValidArtifact:
+  pass
+
+class TLDataTransformArtifact:
+  pass
+
+class TLModelTrainArtifact:
+  pass
+
+class TLModelEvalArtifact:
+  pass
+
+class TLModelPushArtifact:
+  pass
+class TLClassficationMetricArtifact:
+  pass
---
+++ Threat_Lens/Entity/Config_Entity.py
@@ -0,0 +1,35 @@
+import os
+
+from datetime import datetime
+
+from Threat_Lens.Constants import Training_Pipeline
+
+print(Training_Pipeline.ARTIFACT_DIR)
+
+class TLTrainingPipelineConfig:
+  def __init__(self):
+    pass
+
+class TLDataIngestConfig:
+  def __init__(self):
+    pass
+
+class TLDataValidConfig:
+  def __init__(self):
+    pass
+
+class TLDataTransformConfig:
+  def __init__(self):
+    pass
+
+class TLModelTrainConfig:
+  def __init__(self):
+    pass
+
+class TLModelEvalConfig:
+  def __init__(self):
+    pass
+
+class TLModelPushConfig:
+  def __init__(self):
+    pass
