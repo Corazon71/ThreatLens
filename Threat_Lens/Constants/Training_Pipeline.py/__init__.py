caae4811feb57b4cdc925641f0944c2e32731eee
corazon71 <gurunaths6102003@gmail.com>
2024-08-20 08:15:07 +0530

    Constants Configured

---
+++ Threat_Lens/Constants/Training_Pipeline.py/__init__.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+# Common Constants
+TARGET_COLUMN = "Result"
+PIPELINE_NAME : str = "ThreatLens"
+ARTIFACT_DIR : str = "Artifacts"
+FILE_NAME : str = "Data.csv"
+
+TRAIN_FILE_NAME : str = "Train.csv"
+TEST_FILE_NAME : str = "Test.csv"
+
+PREPROCESSING_OBJ_FILE_NAME = "Preprocessing.pkl"
+MODEL_FILE_NAME = "Model.pkl"
+SCHEMA_FILE_PATH = os.path.join("Data_Schema", "Schema.yaml")
+SCHEMA_DROP_COLUMNS = "Drop_Clmn"
+SAVED_MODEL_DIR = os.path.join("Saved_Models")
+
+# Data Ingestion Constants
+DATA_INGESTION_COLLECTION_NAME : str = "TLData"
+DATA_INGESTION_DATABASE_NAME : str = "PunkRecords"
+DATA_INGESTION_DIR_NAME : str = "Data_Ingestion"
+DATA_INGESTION_FEATURESTORE_DIR_NAME : str = "Feature_Store"
+DATA_INGESTION_INGESTED_DIR_NAME : str = "Ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO : float = 0.2
+
+# Data Validation Constants
+
+# Data Transformation Constants
+
+# Model Training Constants
--- Threat_Lens/Entity/Artifact_Entity.py
+++ Threat_Lens/Entity/Artifact_Entity.py
@@ -4,19 +4,25 @@ from dataclasses import dataclass
 class TLDataIngestArtifact:
   pass
 
+@dataclass
 class TLDataValidArtifact:
   pass
 
+@dataclass
 class TLDataTransformArtifact:
   pass
 
+@dataclass
 class TLModelTrainArtifact:
   pass
 
+@dataclass
 class TLModelEvalArtifact:
   pass
 
+@dataclass
 class TLModelPushArtifact:
   pass
+@dataclass
 class TLClassficationMetricArtifact:
   pass
