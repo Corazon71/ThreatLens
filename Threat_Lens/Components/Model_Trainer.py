9a96f2be16c4e6c9b479567c726f69c493c757a7
corazon71 <gurunaths6102003@gmail.com>
2024-09-07 08:11:23 +0530

    Imports for Model Trainer

--- Threat_Lens/Components/Model_Trainer.py
+++ Threat_Lens/Components/Model_Trainer.py
@@ -1,3 +1,14 @@
+import os
+import sys
+
+from Threat_Lens.Exception.Exception import TLException
+from Threat_Lens.Logger.Logger import logging
+
+from xgboost import XGBClassifier
+from Threat_Lens.Utils.ML_Utils import TLModel
+from Threat_Lens.Utils.Main_Utils.Utils import Save_object, Load_object
+from Threat_Lens.Utils.Main_Utils.Utils import Load_numpy_array
+
 class TLModelTrain:
   def __init__(self):
     pass
