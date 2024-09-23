1e8216bf8b150fd25cea52acd6ea94d356eb2e49
corazon71 <gurunaths6102003@gmail.com>
2024-09-23 14:24:05 +0530

    Continuous Deployment Valid

--- Airflow/dags/Training_PL.py
+++ Airflow/dags/Training_PL.py
@@ -30,7 +30,7 @@ with DAG(
   def Sync_Artifact_to_S3Bucket(**kwargs):
     Bkt_name = os.getenv("BUCKET_NAME")
     os.system(f"aws s3 sync /App/Artifact s3://{Bkt_name}/Artifact")
-    os.system(f"aws s3 sync /App/Saved_Moddels s3://{Bkt_name}/Saved_Models")
+    os.system(f"aws s3 sync /App/Saved_Models s3://{Bkt_name}/Saved_Models")
 
   Training_PL = PythonOperator(
     task_id = "train_PL",
